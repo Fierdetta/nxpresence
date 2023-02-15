f9815dae46abb675da0550db9dfe49088f02a733
Jack Matthews <jm5112356@gmail.com>
2023-02-15 00:45:48 -0500

    Default values for settings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
+import { storage } from "@vendetta/plugin";
 import Settings from "./components/Settings";
 
 export default {
     onLoad: () => {
+        storage.presenceApiUrl ??= ""
+        storage.automaticallyConnect ??= false
 
     },
     onUnload: () => {
