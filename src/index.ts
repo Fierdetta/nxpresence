d26bf794a3cf55e03c229e2c10ea5f063ad3031f
Jack Matthews <jm5112356@gmail.com>
2023-02-15 02:33:02 -0500

    Add rudimentary presence stream

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
+import { ReactNative } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
+import { setupNxapiPresenceStream } from "./lib/nxapiPresenceStream";
 import Settings from "./ui/pages/Settings";
+
+let presenceStream;
 
 export default {
     onLoad: () => {
         storage.presenceApiUrl ??= ""
         storage.automaticallyConnect ??= false
 
+        if (storage.automaticallyConnect) {
+            try {
+                presenceStream = setupNxapiPresenceStream()
+            } catch (e) { }
+            // stop and start the stream when app state changes
+            ReactNative.AppState.addEventListener("change", (state) => {
+                if (state === "background") {
+                    if (presenceStream) {
+                        presenceStream.close()
+                    }
+                } else if (state === "active") {
+                    try {
+                        presenceStream = setupNxapiPresenceStream()
+                    } catch (e) { }
+                }
+            })
+        }
+
     },
     onUnload: () => {
-
+        if (presenceStream) {
+            presenceStream.close()
+        }
     },
     settings: Settings,
 }
---
+++ src/lib/nxapiPresenceStream.ts
@@ -0,0 +1,40 @@
+import { storage } from '@vendetta/plugin';
+import { NativeEventSource, EventSourcePolyfill } from 'event-source-polyfill';
+import { setActivity, clearActivity } from './activity';
+import { USER_AGENT } from './constants';
+
+const EventSource = NativeEventSource || EventSourcePolyfill;
+
+export function setupNxapiPresenceStream() {
+	const headers = new Headers({
+		"User-Agent": USER_AGENT
+	})
+
+	let presenceStream = new EventSource(storage.presenceApiUrl + "/events", { headers: headers })
+
+	presenceStream.addEventListener('title', (event) => {
+		const title = JSON.parse(event.data)
+		
+		if (!title) return clearActivity()
+
+		const since = new Date(title.since).getTime()
+
+		return setActivity({
+			name: title.name,
+			type: 0,
+			created_at: since,
+			timestamps: {
+				start: since
+			},
+			assets: {
+				large_image: title.image_url
+			}
+		})
+	});
+
+	presenceStream.addEventListener('close', (event) => {
+		clearActivity()
+	});
+
+	return presenceStream;
+}
